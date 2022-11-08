f8991ceed5a4ceaa01890c932b9d66fd7bce25a4
Metehan <metehan.bolat.ie@gmail.com>
2022-11-08 23:03:19 +0300

    * Sign up to firestore process was completed.

--- app/src/main/java/com/metehanbolat/healthyweight/repository/firestore/FirestoreRepository.kt
+++ app/src/main/java/com/metehanbolat/healthyweight/repository/firestore/FirestoreRepository.kt
@@ -1,5 +1,8 @@
 package com.metehanbolat.healthyweight.repository.firestore
 
-interface FirestoreRepository {
+import com.metehanbolat.healthyweight.model.auth.Member
+import com.metehanbolat.healthyweight.util.UiState
 
+interface FirestoreRepository {
+    fun signUpMember(member: Member, result: (UiState<Member>) -> Unit)
 }
--- app/src/main/java/com/metehanbolat/healthyweight/repository/firestore/FirestoreRepositoryImpl.kt
+++ app/src/main/java/com/metehanbolat/healthyweight/repository/firestore/FirestoreRepositoryImpl.kt
@@ -1,9 +1,20 @@
 package com.metehanbolat.healthyweight.repository.firestore
 
 import com.google.firebase.firestore.FirebaseFirestore
+import com.metehanbolat.healthyweight.model.auth.Member
+import com.metehanbolat.healthyweight.util.UiState
 
 class FirestoreRepositoryImpl(
     val firestore: FirebaseFirestore
 ) : FirestoreRepository {
 
+    override fun signUpMember(member: Member, result: (UiState<Member>) -> Unit) {
+        firestore.collection("members").document().set(member)
+            .addOnSuccessListener {
+                result.invoke(UiState.Success(member))
+            }
+            .addOnFailureListener { failure ->
+                result.invoke(UiState.Failure(failure.localizedMessage))
+            }
+    }
 }
