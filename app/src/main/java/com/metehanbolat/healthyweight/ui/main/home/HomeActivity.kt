eb840fa21e8e51288a60cd58c7d75281fcab3eda
Metehan <metehan.bolat.ie@gmail.com>
2022-11-10 00:45:15 +0300

    * Bottom Navigation Menu was completed.

--- app/src/main/java/com/metehanbolat/healthyweight/ui/main/home/HomeActivity.kt
+++ app/src/main/java/com/metehanbolat/healthyweight/ui/main/home/HomeActivity.kt
@@ -4,7 +4,10 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.snackbar.Snackbar
+import com.metehanbolat.healthyweight.R
 import com.metehanbolat.healthyweight.databinding.ActivityHomeBinding
 import com.metehanbolat.healthyweight.ui.login.sign_in.SignInActivity
 import dagger.hilt.android.AndroidEntryPoint
@@ -22,9 +25,9 @@ class HomeActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
-        binding.exitButton.setOnClickListener {
-            viewModel.signOut()
-        }
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navController = navHostFragment.navController
+        binding.bottomNavigation.setupWithNavController(navController)
 
         viewModel.signOut.observe(this) {
             Snackbar.make(binding.root, it, Snackbar.LENGTH_SHORT).show()
